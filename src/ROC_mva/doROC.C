23fd54a167af6e9d5bc5842a323e03aac3709c59
vtavolar <>
2013-03-04 11:50:21 +0000

    Polite version

--- src/ROC_mva/doROC.C
+++ src/ROC_mva/doROC.C
@@ -1,6 +1,6 @@
 {
-  TFile* stocazzo = new TFile("/xrootdfs/cms/local/gdimperi/Higgs/reduced/redntp.53xv2.cicpfloose.noCorrections.singleGamma_v2_noPresel/merged/redntp_GJet_Pt40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1.root");
-  TTree* tree = (TTree*)stocazzo->Get("AnaTree");
+  TFile* f = new TFile("/xrootdfs/cms/local/gdimperi/Higgs/reduced/redntp.53xv2.cicpfloose.noCorrections.singleGamma_v2_noPresel/merged/redntp_GJet_Pt40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1.root");
+  TTree* tree = (TTree*)f->Get("AnaTree");
   gROOT->ProcessLine(".L tree_reader.C++");
   gROOT->ProcessLine(".L MyEvaluateMVA.cc++");
   gROOT->ProcessLine(".L ROC_curve.C++");
