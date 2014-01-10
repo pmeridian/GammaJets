3462f2e70f24c3da59dc5af2cbb92a2e92f80d37
Paolo Meridiani <paolo.meridiani@cern.ch>
2014-01-10 10:16:00 +0100

    Reverting to paolo_devel_effAccCorr branch GammaJetAnalysis

--- src/FinalTree.h
+++ src/FinalTree.h
@@ -7,8 +7,16 @@ Float_t          finalTree_weight;
 Float_t          finalTree_rho;
 
 Float_t         finalTree_ptPhot;
+Int_t           finalTree_isPreselectedPhot;
+Int_t           finalTree_isSelectedPhot;
 Int_t           finalTree_isMatchedPhot;
 Float_t         finalTree_etaPhot;
 Float_t         finalTree_mvaIdPhot;
 Float_t         finalTree_setaetaPhot;
 Float_t         finalTree_combinedPfIso03Phot;
+
+Float_t         finalTree_ptPhotGen;
+Float_t         finalTree_etaPhotGen;
+Float_t         finalTree_iso03PhotGen;
+Float_t         finalTree_iso04PhotGen;
+Int_t           finalTree_isRecoMatchedPhotGen;
